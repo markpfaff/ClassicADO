c43c7090b6cf9b3771b653874b83fa2883458913
Mark Pfaff <markdpfaff@gmail.com>
2016-01-18 00:52:01 -0800

    Adds web form with drop down list accessing database

---
+++ App_Code/AuthorsAndBooks.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+/// <summary>
+/// This class has a constructor that creates the connection 
+/// object and three methods. 
+/// 
+/// GetAuthors method sets up the SQL 
+/// string to access services and serviceKey. 
+/// 
+/// GetAuthorBooks method does the same but the sql string 
+/// has a variable.
+/// 
+/// The Process Query Method executes the sql and returns
+/// a datatable.
+/// </summary>
+public class AuthorsAndBooks
+{
+    //create connection object
+    SqlConnection connect;
+    public AuthorsAndBooks()
+    {
+        //initialize connection object
+        //the connection string is in the web config
+        //The config manager lets the code
+        //access the web config file
+        connect = new SqlConnection(ConfigurationManager.
+            ConnectionStrings["BookReviewDBConnectionString"].ToString());
+    }
+
+    public DataTable GetAuthors()
+    {
+        //create sql string
+        string sql = "Select AuthorKey, AuthorName from Author";
+        DataTable tbl;//declare table
+        //initialize the command passing sql and connection
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        try
+        {
+            //call ProcessQuery method
+            tbl = ProcessQuery(cmd);
+        }
+        catch (Exception ex)
+        {
+            //throw it back to the form
+            throw ex;
+        }
+        return tbl;
+
+    }
+    public DataTable GetAuthorBooks(int authorKey)
+    {
+        string sql = "SELECT * FROM Book INNER JOIN AuthorBook ON Book.BookKey = AuthorBook.BookKey WHERE AuthorKey = @AuthorKey";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        //add a parameter to store the variable from 
+        //the drop down list
+        cmd.Parameters.Add("@AuthorKey", authorKey);
+
+        DataTable tbl;
+        try
+        {
+            tbl = ProcessQuery(cmd);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return tbl;
+
+    }
+
+    private DataTable ProcessQuery(SqlCommand cmd)
+    {
+        //This method processes the queries
+        DataTable table = new DataTable();
+        SqlDataReader reader;
+        try
+        {
+            connect.Open();
+            reader = cmd.ExecuteReader();
+            table.Load(reader);
+            connect.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return table;
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//add this
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //declare the AuthorsAndBooks class so it is available
+    //to all methods
+    AuthorsAndBooks gs = new AuthorsAndBooks();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //call the FillDropDownList Method if it is not a postback
+        if (!IsPostBack)
+            FillDropDownList();
+    }
+    protected void AuthorDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //when the index of the selected item in the
+        //drop down box changes call the FillGridViewMethod
+        FillGridView();
+    }
+
+    protected void FillDropDownList()
+    {
+        //Declare the dataTabe
+        DataTable table = null; ;
+        try
+        {
+            //call the method in the class to fill the table
+            table = gs.GetAuthors();
+        }
+        catch (Exception ex)
+        {
+            ErrorLabel.Text = ex.Message;
+        }
+        //Attach the table as a datasource for the 
+        //drop down list
+        //assign the display and value fields
+        AuthorDropDownList.DataSource = table;
+        AuthorDropDownList.DataTextField = "AuthorName";
+        AuthorDropDownList.DataValueField = "AuthorKey";
+        AuthorDropDownList.DataBind();
+    }
+
+    protected void FillGridView()
+    {
+        //Get the value from the drop down list
+        //selected value
+        int authorKey = int.Parse(AuthorDropDownList.SelectedValue.ToString());
+        DataTable tbl = null;
+        try
+        {
+            //call the GetAuthorBooks field and pass 
+            //it the service key
+            tbl = gs.GetAuthorBooks(authorKey);
+
+        }
+        catch (Exception ex)
+        {
+            ErrorLabel.Text = ex.Message;
+        }
+
+        //attach the table as a data source to the
+        //gridView
+        BooksGridView.DataSource = tbl;
+        BooksGridView.DataBind();
+    }
+}
